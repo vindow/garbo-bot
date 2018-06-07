5cf3c23c6db491d0951733257524545a28cef197
Alvin Yang <alvin.yang73@gmail.com>
2018-06-07 16:40:02 -0700

    Moved cogs into their own directory.

--- bot.py
+++ bot.py
@@ -1,9 +1,11 @@
 from discord import Game
 from discord.ext import commands
+from os import listdir
+from os.path import isfile, join
 
 client = commands.Bot(command_prefix="!")
 TOKEN_ID = "NDUxNTkxNTQ4MDYxMzUxOTM2.DfECOQ.T1270Rae2W0E8QgM2E3G8-fdeaM"
-startup_extensions = ["rng", "dota"]
+cogs_dir = "cogs"
 
 
 @client.event
@@ -12,7 +14,7 @@ async def on_ready():
     print("logged in as " + client.user.name + "#" + client.user.id)
 
 
-@client.command(hidden=True)
+@client.command()
 async def load(extension_name : str):
     """Loads an extension."""
     try:
@@ -23,28 +25,17 @@ async def load(extension_name : str):
     await client.say("{} loaded.".format(extension_name))
 
 
-@client.command(hidden=True)
+@client.command()
 async def unload(extension_name : str):
     """Unloads an extension."""
     client.unload_extension(extension_name)
     await client.say("{} unloaded.".format(extension_name))
 
 
-@client.event
-async def on_message(message):
-    # we do not want the bot to reply to itself
-    if message.author == client.user:
-        return
-
-    if "brad sucks" in message.content.lower():
-        await client.send_message(message.channel, "I concur.")
-
-
 if __name__ == "__main__":
-    for extension in startup_extensions:
+    for extension in [f.replace('.py', '') for f in listdir(cogs_dir) if isfile(join(cogs_dir, f))]:
         try:
-            client.load_extension(extension)
+            client.load_extension(cogs_dir + "." + extension)
         except Exception as e:
-            exc = '{}: {}'.format(type(e).__name__, e)
-            print('Failed to load extension {}\n{}'.format(extension, exc))
+            print(f'Failed to load extension {extension}.')
     client.run(TOKEN_ID)
