4ebc971b1f2461f78e58aafe6e95368b7cfa8430
Alvin Yang <alvin.yang73@gmail.com>
2018-06-09 15:19:03 -0700

    Added poll features to bot. Updated README with bot features.

--- README.md
+++ README.md
@@ -4,4 +4,8 @@ Just a personal bot for a personal discord server.
 
 ## Features
 
-TBD
+* !roll — Rolls a psuedorandom number defaulted to 1-100 but can be set to any range with extra parameters.
+* !8ball — Gives 1 of 20 magic 8-ball responses (see: https://en.wikipedia.org/wiki/Magic_8-Ball)
+* !rhero — Picks a random Dota 2 hero.
+* !poll — Creates a basic yes/no poll by posting the question and added a thumbs up and thumbs down reaction.
+* !strawpoll — Creates a strawpoll on strawpoll.me using the given parameters and posts it.
---
+++ cogs/poll.py
@@ -0,0 +1,53 @@
+from discord.ext import commands
+import requests
+import re
+import json
+
+
+class Poll:
+    def __init__(self, bot):
+        self.client = bot
+
+    @commands.command(name='poll',
+                      description="Creates a yes/no poll for a given question. Format should be !poll {question}",
+                      brief="Create a basic yes/no poll.",
+                      pass_context=True,
+                      no_pm=True)
+    async def poll(self, context):
+        message = context.message.content[6:]
+        # Check for valid input
+        if not(message[1].startswith('{') and message[1].endswith('}')):
+            await self.client.say("Invalid input for `!poll`. See `!help poll` for help.")
+        else:
+            question = message[1]
+            poll_message = await self.client.say(question[1:-1])
+            await self.client.add_reaction(poll_message, "👍")
+            await self.client.add_reaction(poll_message, "👎")
+
+    @commands.command(name="strawpoll",
+                      description="Creates a strawpoll based on the given inputs."
+                                  "Format should be !strawpoll {title} [option 1] [option 2] [option 3] etc",
+                      brief="Create a strawpoll.",
+                      pass_context=True,
+                      no_pm=True)
+    async def strawpoll(self, context):
+        message = context.message.content
+        if not re.fullmatch("!strawpoll \{\w+( +\w+)*\}( \[\w+( +\w+)*\]){2,}", message):
+            await self.client.say("Invalid input for `!strawpoll`. See `!help strawpoll` for help.")
+            return
+        else:
+            title_start = message.find("{") + 1
+            title_end = message.find("}")
+            title = message[title_start:title_end]
+            options_start = title_end + 3
+            options_end = len(message) - 1
+            options = message[options_start:options_end].split("] [")
+            payload = {'title': title, 'options': options}
+            headers = {'Content-type': 'application/json'}
+            strawpoll_url = "https://www.strawpoll.me/api/v2/polls"
+            response = requests.post(strawpoll_url, data=json.dumps(payload), headers=headers).json()
+            await self.client.say("Strawpoll: https://www.strawpoll.me/" + str(response['id']))
+
+
+def setup(bot):
+    bot.add_cog(Poll(bot))
